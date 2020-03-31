50ca20e3f4e147d21bbe6cd88923933a450cfc00
Rohan-vs <32184195+Rohan-vs@users.noreply.github.com>
2020-03-31 00:18:15 -0400

    Update kinter.py

--- kinter.py
+++ kinter.py
@@ -1,13 +1,9 @@
-import subprocess
 from tkinter import *
 from tkinter import filedialog
 from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
 import shutil
 import os
-import pathlib
-from pydub import AudioSegment
 from pytube import YouTube
-import time
 
 
 fPath = os.getcwd() + os.sep
@@ -15,15 +11,6 @@ fPath = os.getcwd() + os.sep
 def file_path(file_name):
     # return os.path.dirname(os.path.realpath(__file__)) + os.sep + file_name
     return os.getcwd() + os.sep + file_name
-
-
-# def runCommand(command):
-#     out = subprocess.Popen(command.split(), shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-#     stdout, stderr = out.communicate()
-#     out = stdout.decode('utf-8')
-#     text.insert(END, out)
-#     print(out)
-
 
 def download():
     import moviepy.editor as mp
@@ -35,11 +22,8 @@ def download():
     album = albumEntry.get()
 
     yt = YouTube(link)
-    # file_name = str(int(time.time()))
     file_name = title
     yt.streams.filter(file_extension='mp4').first().download(filename=file_name)
-
-
 
     fileTitle4 = file_name + '.mp4'
     fileTitle3 = file_name + '.mp3'
@@ -48,42 +32,19 @@ def download():
 
     clip.audio.write_audiofile(fileTitle3)
 
-    # print(fileTitle3 + "=filetitle")
-    # print(MP3File.url)
-    # mp3 = MP3File(file_path(fileTitle3))
-    # # mp3 = MP3File(current+fileTitle3)
-    # print(album)
-    # print(artist)
-    # print(title)
-    #
-    # mp3.album = album
-    # mp3.artist = artist
-    # mp3.song = title
-    # mp3.save()
-    # meta(album, artist, title, fileTitle3)
-    from kinter2 import woo
-    k = woo(title, album, artist)
-    k.dl()
-
-
-    # os.remove(file_path(fileTitle4))
-
-
-    shutil.move(current+fileTitle3, fPathh + os.sep + fileTitle3)
-
-def meta(album, artist, title, fileTitle3):
     mp3 = MP3File(file_path(fileTitle3))
     mp3.album = album
     mp3.artist = artist
     mp3.song = title
-    mp3.save()
+    mp3.save()    
+    # os.remove(file_path(fileTitle4))
+
+    shutil.move(current+fileTitle3, fPath + os.sep + fileTitle3)
 
 def filePath():
-    global fPathh
-    fPathh=filedialog.askdirectory() + os.sep
-    pathLabel.config(text=fPathh)
-
-
+    global fPath
+    fPath=filedialog.askdirectory() + os.sep
+    pathLabel.config(text=fPath)
 
 root = Tk()
 root.title("this is tkinter")
@@ -102,8 +63,6 @@ artistLabel = Label(tFrame, text="Artist: ")
 albumLabel = Label(tFrame, text="Album: ")
 outputLabel = Label(tFrame, text="Output: ")
 pathLabel = Label(tFrame, text=fPath)
-
-
 
 linkLabel.grid(row=0, sticky=W)
 titleLabel.grid(row=1, sticky=W)
@@ -136,7 +95,6 @@ titleEntry.config(width=ew)
 artistEntry.config(width=ew)
 albumEntry.config(width=ew)
 
-
 dlBtn = Button(tFrame, text="download", command=download)
 dlBtn.grid(row=4, column=1, sticky=E)
 
@@ -146,8 +104,5 @@ fPath.grid(row=2,column=2,sticky=E)
 # text = Text(bFrame, height=20, width=100)
 # text.grid(row=0, padx=5)
 # text.insert(END, "")
-
-
-
 
 root.mainloop()
